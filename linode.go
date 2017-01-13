3e6c515796fca051af50054a9a845bca1edab185
Abe Massry <a@abemassry.com>
2017-01-13 17:51:52 -0500

    Update readme (#1)
    * update README.md and style for go
    
    * markdown formatting
    
    * more markdown formatting

--- linode.go
+++ linode.go
@@ -5,177 +5,177 @@ import "net/http"
 import "encoding/json"
 
 type LinodeClient struct {
-    Token string
+	Token string
 }
 
-func (c LinodeClient) Request(snippet string, result interface{}) (error) {
-    endpoint := "https://api.alpha.linode.com/v4/"
+func (c LinodeClient) Request(snippet string, result interface{}) error {
+	endpoint := "https://api.alpha.linode.com/v4/"
 
-    client := &http.Client{}
+	client := &http.Client{}
 
-    req, err := http.NewRequest("GET", endpoint + snippet, nil)
-    req.Header.Add("Authorization", "token " + c.Token)
-    resp, err := client.Do(req)
+	req, err := http.NewRequest("GET", endpoint+snippet, nil)
+	req.Header.Add("Authorization", "token "+c.Token)
+	resp, err := client.Do(req)
 
-    if (err != nil) {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    err = json.NewDecoder(resp.Body).Decode(&result)
+	err = json.NewDecoder(resp.Body).Decode(&result)
 
-    if (err != nil) {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 type Datacenter struct {
-    ID      string `json:"id"`
-    Label   string `json:"label"`
-    Country string `json:"country"`
+	ID      string `json:"id"`
+	Label   string `json:"label"`
+	Country string `json:"country"`
 }
 
 type DatacentersResult struct {
-    TotalPages   uint         `json:"total_pages"`
-    TotalResults uint         `json:"total_results"`
-    Page         uint         `json:"page"`
-    Datacenters  []Datacenter `json:"datacenters"`
+	TotalPages   uint         `json:"total_pages"`
+	TotalResults uint         `json:"total_results"`
+	Page         uint         `json:"page"`
+	Datacenters  []Datacenter `json:"datacenters"`
 }
 
 type Time struct {
-    time.Time
+	time.Time
 }
 
 func (t *Time) UnmarshalJSON(b []byte) error {
-    // TODO: actually parse the time
-    *t = Time{time.Now()}
-    return nil
+	// TODO: actually parse the time
+	*t = Time{time.Now()}
+	return nil
 }
 
 type Distribution struct {
-    ID                 string    `json:"id"`
-    Created            Time      `json:"created"`
-    Label              string    `json:"label"`
-    MinimumStorageSize uint      `json:"minimum_storage_size"`
-    Recommended        bool      `json:"recommended"`
-    Vendor             string    `json:"vendor"`
-    X64                bool      `json:"X64"`
+	ID                 string `json:"id"`
+	Created            Time   `json:"created"`
+	Label              string `json:"label"`
+	MinimumStorageSize uint   `json:"minimum_storage_size"`
+	Recommended        bool   `json:"recommended"`
+	Vendor             string `json:"vendor"`
+	X64                bool   `json:"X64"`
 }
 
 type DistributionResult struct {
-    TotalPages    uint           `json:"total_pages"`
-    TotalResults  uint           `json:"total_results"`
-    Page          uint           `json:"page"`
-    Distributions []Distribution `json:"distributions"`
+	TotalPages    uint           `json:"total_pages"`
+	TotalResults  uint           `json:"total_results"`
+	Page          uint           `json:"page"`
+	Distributions []Distribution `json:"distributions"`
 }
 
 type Kernel struct {
-    ID          string `json:"id"`
-    Created     Time   `json:"created"`
-    Deprecated  bool   `json:"deprecated"`
-    Description string `json:"description"`
-    Xen         bool   `json:"xen"`
-    KVM         bool   `json:"kvm"`
-    Label       string `json:"label"`
-    Version     string `json:"version"`
-    X64         bool   `json:"x64"`
+	ID          string `json:"id"`
+	Created     Time   `json:"created"`
+	Deprecated  bool   `json:"deprecated"`
+	Description string `json:"description"`
+	Xen         bool   `json:"xen"`
+	KVM         bool   `json:"kvm"`
+	Label       string `json:"label"`
+	Version     string `json:"version"`
+	X64         bool   `json:"x64"`
 }
 
 type KernelResult struct {
-    TotalPages   uint     `json:"total_pages"`
-    TotalResults uint     `json:"total_results"`
-    Page         uint     `json:"page"`
-    Kernels      []Kernel `json:"kernels"`
+	TotalPages   uint     `json:"total_pages"`
+	TotalResults uint     `json:"total_results"`
+	Page         uint     `json:"page"`
+	Kernels      []Kernel `json:"kernels"`
 }
 
 type Alert struct {
-    Enabled   bool `json:"enabled"`
-    Threshold uint `json:"threshold"`
+	Enabled   bool `json:"enabled"`
+	Threshold uint `json:"threshold"`
 }
 
 type IPv4 struct {
-    Address string `json:"address"`
-    Type    string `json:"type"`
-    RDNS    string `json:"rdns"`
+	Address string `json:"address"`
+	Type    string `json:"type"`
+	RDNS    string `json:"rdns"`
 }
 
 type IPv6 struct {
-    Range string `json:"range"`
-    Type  string `json:"type"`
+	Range string `json:"range"`
+	Type  string `json:"type"`
 }
 
 type Linode struct {
-    ID            uint             `json:"id"`
-    Alerts        map[string]Alert `json:"alerts"`
-    Backups       BackupInfo       `json:"backups"`
-    Created       Time             `json:"created"`
-    Datacenter    Datacenter       `json:"datacenter"`
-    Distribution  Distribution     `json:"distribution"`
-    Group         string           `json:"group"`
-    IPv4          string           `json:"ipv4"`
-    IPv6          string           `json:"ipv6"`
-    Label         string           `json:"label"`
-    Type          []Service        `json:"type"`
-    Status        string           `json:"status"`
-    TotalTransfer uint             `json:"total_transfer"`
-    Updated       Time             `json:"updated"`
+	ID            uint             `json:"id"`
+	Alerts        map[string]Alert `json:"alerts"`
+	Backups       BackupInfo       `json:"backups"`
+	Created       Time             `json:"created"`
+	Datacenter    Datacenter       `json:"datacenter"`
+	Distribution  Distribution     `json:"distribution"`
+	Group         string           `json:"group"`
+	IPv4          string           `json:"ipv4"`
+	IPv6          string           `json:"ipv6"`
+	Label         string           `json:"label"`
+	Type          []Service        `json:"type"`
+	Status        string           `json:"status"`
+	TotalTransfer uint             `json:"total_transfer"`
+	Updated       Time             `json:"updated"`
 }
 
 type Disk struct {
-    ID         uint   `json:"id"`
-    Label      string `json:"label"`
-    Status     string `json:"status"`
-    Size       uint   `json:"size"`
-    Filesystem string `json:"filesystem"`
-    Created    Time   `json:"created"`
-    Updated    Time   `json:"updated"`
+	ID         uint   `json:"id"`
+	Label      string `json:"label"`
+	Status     string `json:"status"`
+	Size       uint   `json:"size"`
+	Filesystem string `json:"filesystem"`
+	Created    Time   `json:"created"`
+	Updated    Time   `json:"updated"`
 }
 
 type Backup struct {
-    ID         uint       `json:"id"`
-    Label      string     `json:"label"`
-    Status     string     `json:"status"`
-    Type       string     `json:"type"`
-    Datacenter Datacenter `json:"datacenter"`
-    Created    Time       `json:"created"`
-    Updated    Time       `json:"updated"`
-    Finished   Time       `json:"finished"`
-    Configs    []string   `json:"configs"`
-    Disks      []Disk     `json:"disks"`
+	ID         uint       `json:"id"`
+	Label      string     `json:"label"`
+	Status     string     `json:"status"`
+	Type       string     `json:"type"`
+	Datacenter Datacenter `json:"datacenter"`
+	Created    Time       `json:"created"`
+	Updated    Time       `json:"updated"`
+	Finished   Time       `json:"finished"`
+	Configs    []string   `json:"configs"`
+	Disks      []Disk     `json:"disks"`
 }
 
 type Schedule struct {
-    Day    string `json:"day"`
-    Window string `json:"window"`
+	Day    string `json:"day"`
+	Window string `json:"window"`
 }
 
 type BackupInfo struct {
-    Enabled    bool     `json:"enabled"`
-    Schedule   Schedule `json:"schedule"`
-    LastBackup Backup   `json:"last_backup"`
-    Snapshot   Backup   `json:"snapshot"`
+	Enabled    bool     `json:"enabled"`
+	Schedule   Schedule `json:"schedule"`
+	LastBackup Backup   `json:"last_backup"`
+	Snapshot   Backup   `json:"snapshot"`
 }
 
 type Service struct {
-    ID           string      `json:"id"`
-    Storage      uint        `json:"storage"`
-    BackupsPrice uint        `json:"backups_price"`
-    HourlyPrice  uint        `json:"hourly_price"`
-    Label        string      `json:"label"`
-    MBitsOut     uint        `json:"mbits_out"`
-    MonthlyPrice uint        `json:"monthly_price"`
-    RAM          uint        `json:"ram"`
-    ServiceType  string      `json:"service_type"`
-    Transfer     uint        `json:"transfer"`
-    VCPUs        uint        `json:"vcpus"`
+	ID           string `json:"id"`
+	Storage      uint   `json:"storage"`
+	BackupsPrice uint   `json:"backups_price"`
+	HourlyPrice  uint   `json:"hourly_price"`
+	Label        string `json:"label"`
+	MBitsOut     uint   `json:"mbits_out"`
+	MonthlyPrice uint   `json:"monthly_price"`
+	RAM          uint   `json:"ram"`
+	ServiceType  string `json:"service_type"`
+	Transfer     uint   `json:"transfer"`
+	VCPUs        uint   `json:"vcpus"`
 }
 
 type LinodeResult struct {
-    TotalPages   uint     `json:"total_pages"`
-    TotalResults uint     `json:"total_results"`
-    Page         uint     `json:"page"`
-    Linodes      []Linode `json:"linodes"`
+	TotalPages   uint     `json:"total_pages"`
+	TotalResults uint     `json:"total_results"`
+	Page         uint     `json:"page"`
+	Linodes      []Linode `json:"linodes"`
 }
--- test.go
+++ test.go
@@ -6,34 +6,34 @@ import "strings"
 import "io/ioutil"
 
 func main() {
-    token, err := ioutil.ReadFile("linode-token")
+	token, err := ioutil.ReadFile("linode-token")
 
-    if (err != nil) {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    linode := LinodeClient{strings.TrimSpace(string(token))}
+	linode := LinodeClient{strings.TrimSpace(string(token))}
 
-    var result LinodeResult
-    err = linode.Request("linode/instances", &result)
+	var result LinodeResult
+	err = linode.Request("linode/instances", &result)
 
-    //var result LinodeResult
-    //err := request("linode/instances", &result)
+	//var result LinodeResult
+	//err := request("linode/instances", &result)
 
-    //var result DatacentersResult
-    //err := linode.Request("datacenters", &result)
+	//var result DatacentersResult
+	//err := linode.Request("datacenters", &result)
 
-    //var result DistributionResult
-    //err := request("distributions", &result)
+	//var result DistributionResult
+	//err := request("distributions", &result)
 
-    //var result KernelResult
-    //err := request("kernels", &result)
+	//var result KernelResult
+	//err := request("kernels", &result)
 
-    if (err != nil) {
-        log.Fatal(err)
-        return
-    }
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
-    fmt.Println(result)
-    fmt.Println(result.TotalPages)
+	fmt.Println(result)
+	fmt.Println(result.TotalPages)
 }
